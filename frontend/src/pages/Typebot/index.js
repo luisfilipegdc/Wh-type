085a8d68304d508bb86b75a72f0a9747cd9463e1
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2025-01-25 19:36:13 -0300

    icio

---
+++ frontend/src/pages/Typebot/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  mainPaper: {
+    flex: 1,
+    padding: theme.spacing(1),
+    overflowY: "scroll",
+    ...theme.scrollbarStyles,
+  },
+  iframe: {
+    border: "none",
+    //position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+  },
+}));
+
+const Typebot = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.iframe}>
+        <iframe
+          className={classes.iframe}
+          src="https://app.typebot.live/pt-BR/typebots"
+        ></iframe>
+      </div>
+    </>
+  );
+};
+
+export default Typebot;
